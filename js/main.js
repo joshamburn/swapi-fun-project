7272e9ff8b2e080b8f82347d19176f5d9daa3cc2
Josh <josh.amburn@claytonhomes.com>
2019-06-12 15:25:10 -0400

    Temp change to base url for testing

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-const baseUrl = 'https://swapi.co/api/';
+// the api base url for requests
+//const baseUrl = 'https://swapi.co/api/';
+const baseUrl = 'https://fast-everglades-97829.herokuapp.com/homes';
 
+//Creating the request
 const xhr = new XMLHttpRequest();
+
+//Opening the connection
 xhr.open('GET', baseUrl, true);
+
+//???
 xhr.responseType = 'text';
+
+//Send the actual request
 xhr.send();
 
+//Do something with the data
 xhr.onload = function(){
     if(xhr.status === 200) {
         var swapiObj = JSON.parse(xhr.responseText);
